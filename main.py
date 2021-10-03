30ef2fb5db1d66644a9ba2cf64faaf0c4a6446a6
amator <amatoravg@gmail.com>
2021-10-03 15:40:31 +0700

    Add quicksort

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ def div_array(big_list, inv_count):
     length = len(big_list)//2
     l_ar = big_list[:length]
     r_ar = big_list[length:]
-    print(l_ar)
-    print(r_ar)
+    # print(l_ar)
+    # print(r_ar)
     if len(l_ar) > 1:
         l_ar, inv_count = div_array(l_ar, inv_count)
     if len(r_ar) > 1:
@@ -32,28 +32,21 @@ def merge_sort(l_ar, r_ar, inv_count):
         elif l_ind == len_l_ar:
             res.append(r_ar[r_ind])
             r_ind += 1
-        elif l_ar[l_ind] < r_ar[r_ind]:
+        elif l_ar[l_ind] <= r_ar[r_ind]:
             res.append(l_ar[l_ind])
             l_ind += 1
         else:
             res.append(r_ar[r_ind])
             r_ind += 1
-            inv_count += 1
+            inv_count += len_l_ar - l_ind
 
-    # if l_ar < r_ar:
-    #     return [l_ar[0], r_ar[0]]
-    # else:
-    #     return [r_ar[0], l_ar[0]]
-    # print(res)
     return res, inv_count
-
-
-    # return l_ar, r_ar
 
 
 n = int(input.readline())
 col = list(map(int, input.readline().split()))
-print(n)
-print(col)
+# print(n)
+# print(col)
 sort_ar, inv_count = div_array(col, 0)
-print(sort_ar, inv_count)
+# print(sort_ar, inv_count)
+print(inv_count)
